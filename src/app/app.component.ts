4077efb0ae077bfad9f05ab53a8b1f3d0e8a3473
hauszmanndavid <hauszmannd@gmail.com>
2025-12-02 09:22:43 +0100

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,11 @@
 import { Component } from '@angular/core';
-@Component({selector:'app-root',template:'<h1>Angular Stable</h1>'})
+
+
+@Component({
+    selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
 export class AppComponent {}
