69ddd187cf1c4ddc0e5c190ce5def16c5fdae0a5
hauszmanndavid <hauszmannd@gmail.com>
2026-01-15 13:37:25 +0100

    complete product-detail

--- Webshop/src/app/product-detail/product-detail.ts
+++ Webshop/src/app/product-detail/product-detail.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavBar } from "../nav-bar/nav-bar";
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule, NavBar],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
 export class ProductDetail {
 
+  product = {
+    images: [
+      'assets/image1.jpg',
+      'assets/image2.jpg',
+      'assets/image3.jpg'
+    ],
+    price: 2999,
+    description: 'Ez egy részletes leírás a termékről.'
+  };
+  images = this.product.images;
+  currentImageIndex: number = 0;
+
+  nextImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  addToFavorites() {
+    console.log('Hozzáadva a kedvencekhez');
+  }
+
+  addToCart() {
+    console.log('Hozzáadva a kosárhoz');
+  }
 }
